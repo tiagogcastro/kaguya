397caa526935095d92d10f4708a81600f3436628
Tiago Gonçalves <tiaguin180@gmail.com>
2021-08-10 17:48:57 -0300

    refactor: Removed ensure authenticate

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { CreateUserController } from '../controllers/CreateUserController';
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 
-usersRouter.post('/',ensureAuthenticated, createUserController.handle);
+usersRouter.post('/', createUserController.handle);
 
 export { usersRouter };
