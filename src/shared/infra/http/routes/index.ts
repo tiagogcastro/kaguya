9480e20c8edc5251ed6b5628fc7b9688080df56f
Tiago Gonçalves <tiaguin180@gmail.com>
2021-08-10 17:44:12 -0300

    feat: Using admin router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import { usersRouter } from '@modules/users/infra/http/routes/users.routes';
 import { platformRolesRouter } from '@modules/platformRoles/infra/http/routes/platformRoles.routes';
 import { Router } from 'express';
 import { sessionRouter } from '@modules/users/infra/http/routes/session.routes';
+import { adminRouter } from '@modules/users/infra/http/routes/admin.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/admin', adminRouter);
 router.use('/session', sessionRouter);
 
-router.use('/platform/roles', platformRolesRouter);
+router.use('/platform-roles', platformRolesRouter);
 
 export { router };
