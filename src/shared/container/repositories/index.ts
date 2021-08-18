b1e299ac5383991aaba45f35d70b05fad398e28c
Marcos Proença <marcosproenca144@gmail.com>
2021-08-18 20:34:45 -0300

    improve: repositories instances

---
+++ src/shared/container/repositories/index.ts
@@ -0,0 +1,28 @@
+import { IPlatformRolesRepository } from '@modules/platformRoles/domain/repositories/IPlatformRolesRepository';
+import { PlatformRolesRepository } from '@modules/platformRoles/infra/typeorm/repositories/PlatformRolesRepository';
+import { ITrailsRepository } from '@modules/trails/domain/repositories/ITrailsRepository';
+import { TrailsRepository } from '@modules/trails/infra/typeorm/repositories/TrailsRepository';
+import { IPlatformUserRolesRepository } from '@modules/users/domain/repositories/IPlatformUserRolesRepository';
+import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import { PlatformUserRolesRepository } from '@modules/users/infra/typeorm/repositories/PlatformUserRolesRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { container } from 'tsyringe';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+container.registerSingleton<IPlatformUserRolesRepository>(
+  'PlatformUserRolesRepository',
+  PlatformUserRolesRepository,
+);
+
+container.registerSingleton<IPlatformRolesRepository>(
+  'PlatformRolesRepository',
+  PlatformRolesRepository,
+);
+
+container.registerSingleton<ITrailsRepository>(
+  'TrailsRepository',
+  TrailsRepository,
+);
