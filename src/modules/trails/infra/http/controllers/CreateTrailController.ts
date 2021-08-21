31d0f4d4438743a387690e19ca8653bdaa185553
Marcos Proença <marcosproenca144@gmail.com>
2021-08-21 20:59:25 -0300

    feat: interface user trails repository was created to respect ddd and solid

--- src/modules/trails/infra/http/controllers/CreateTrailController.ts
+++ src/modules/trails/infra/http/controllers/CreateTrailController.ts
@@ -1,20 +1,19 @@
+import { classToClass } from 'class-transformer';
 import { CreateTrailService } from '@modules/trails/services/CreateTrailService';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 export class CreateTrailController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const avatar = request.file?.filename;
     const { name, description } = request.body;
 
     const createTrail = container.resolve(CreateTrailService);
 
     const trailCreated = await createTrail.execute({
-      avatar,
       description,
       name,
     });
 
-    return response.status(201).json(trailCreated);
+    return response.status(201).json(classToClass(trailCreated));
   }
 }
