26fd215756c566de046cd4bdad898e1edd7f7f2c
Marcos Proença <marcosproenca144@gmail.com>
2021-08-21 21:00:45 -0300

    improve: four methods were implemented

--- src/modules/trails/infra/http/routes/trails.routes.ts
+++ src/modules/trails/infra/http/routes/trails.routes.ts
@@ -5,10 +5,18 @@ import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
 import multer from 'multer';
 import { CreateTrailController } from '../controllers/CreateTrailController';
+import { DestroyTrailController } from '../controllers/DestroyTrailController';
+import { ListAllTrailsController } from '../controllers/ListAllTrailsController';
+import { UpdateTrailAvatarController } from '../controllers/UpdateTrailAvatarController';
+import { UpdateTrailController } from '../controllers/UpdateTrailController';
 
 const trailsRouter = Router();
 
 const createTrailController = new CreateTrailController();
+const listAllTrailsController = new ListAllTrailsController();
+const destroyTrailController = new DestroyTrailController();
+const updateTrailController = new UpdateTrailController();
+const updateTrailAvatarController = new UpdateTrailAvatarController();
 
 const upload = multer(storageConfig.multer);
 
@@ -16,7 +24,6 @@ trailsRouter.use(ensureAuthenticated, ensureSubAdministrator);
 
 trailsRouter.post(
   '/',
-  upload.single('avatar'),
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().max(100).required(),
@@ -24,6 +31,36 @@ trailsRouter.post(
     },
   }),
   createTrailController.handle,
+);
+
+trailsRouter.get('/list-all', listAllTrailsController.handle);
+
+trailsRouter.delete(
+  '/',
+  celebrate({
+    [Segments.QUERY]: {
+      trail_id: Joi.string().uuid().required(),
+    },
+  }),
+  destroyTrailController.handle,
+);
+
+trailsRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      trail_id: Joi.string().uuid().required(),
+      name: Joi.string().max(100),
+      description: Joi.string().max(255),
+    },
+  }),
+  updateTrailController.handle,
+);
+
+trailsRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  updateTrailAvatarController.handle,
 );
 
 export { trailsRouter };
