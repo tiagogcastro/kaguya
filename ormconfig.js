ac6b582d4230cf3127407ca95a4a100ddd1512e6
Marcos Proença <marcosproenca144@gmail.com>
2021-10-16 15:26:36 -0300

    fix: ssl enabled

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,14 @@
 const enviroment = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
-  "entities": [
-    enviroment === 'production' ?
-    './build/modules/**/infra/typeorm/entities/*.js' :
-    './src/modules/**/infra/typeorm/entities/*.ts'
-  ]
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: [
+    enviroment === 'production'
+    ? './build/modules/**/infra/typeorm/entities/*.js'
+    : './src/modules/**/infra/typeorm/entities/*.ts'
+  ],
+  ssl: enviroment === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 }
