fb4d50a81e92c38ac50091108d8e961f8385f8ff
Marcos Proença <marcosproenca144@gmail.com>
2021-10-24 17:09:29 -0300

    fix: graphql issue was solved

--- src/config/graphql.ts
+++ src/config/graphql.ts
@@ -9,11 +9,14 @@ const rootPath = path.resolve(
   'http',
   'graphql',
 );
-
+const envrionment = process.env.NODE_ENV || 'development';
 const graphQLConfig = {
   paths: {
     typeDefs: path.resolve(rootPath, '*.gql'),
-    resolvers: path.resolve(rootPath, '*.resolvers.ts'),
+    resolvers: path.resolve(
+      rootPath,
+      envrionment === 'production' ? '*.resolvers.js' : '*.resolvers.ts',
+    ),
   },
 };
 
