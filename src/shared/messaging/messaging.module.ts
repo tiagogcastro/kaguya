9cc36a4442ee5bacd0001ab70fd4b93c56da1b9c
Tiaguin061 <tiaguin180@gmail.com>
2023-03-05 12:11:48 -0300

    feat: Created kafka config

---
+++ src/shared/messaging/messaging.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthController } from '@shared/messaging/controllers/auth.controller';
+import { PrismaService } from '@shared/database/prisma/prisma.service';
+
+import { SuggestiveRepository } from '@modules/suggestive/infra/prisma/repositories/suggestive-repository';
+import { CreateSuggestiveService } from '@modules/suggestive/services/create-suggestive.service';
+import { KafkaService } from './kafka.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AuthController],
+  providers: [CreateSuggestiveService, SuggestiveRepository, PrismaService, KafkaService]
+})
+export class MessagingModule {}
