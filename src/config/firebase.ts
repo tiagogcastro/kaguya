469d62fcd955a32ce8fec8ed8277fd0d2470b729
pprograminha <marcosproenca144@gmail.com>
2023-05-21 00:43:42 -0300

    test: console

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -9,6 +9,7 @@ import {
 type ProviderName = 'google' | 'github'
 
 const firebaseConfig: FirebaseOptions = {
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
